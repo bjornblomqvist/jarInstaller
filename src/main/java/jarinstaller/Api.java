ceebb4ad9cb4db2a2d13ea170ba14dae829281ff
Björn Blomqvist <bjorn@bjornblomqvist.se>
2019-01-29 23:02:45 +0100

    Fix javadoc warnings

--- src/main/java/jarinstaller/Api.java
+++ src/main/java/jarinstaller/Api.java
@@ -7,9 +7,9 @@ public class Api {
     
     /**
      * Removes the current jar and script from ~/.jars/
-     * 
-     * @return
-     * @throws JarInstallerException 
+     *
+     * @return true on success
+     * @throws JarInstallerException if there is any exception
      */
     public static boolean unInstall() throws JarInstallerException {
         return jarinstaller.impl.Utils.unInstall(getJarPathAtBottomOfStack());
@@ -17,18 +17,19 @@ public class Api {
     
     /**
      * Copies the current jar to ~/.jars/jars and creates a bash script in ~/.jars/bin
-     * 
-     * @return
-     * @throws JarInstallerException 
+     *
+     * @return true on success
+     * @throws JarInstallerException if there is any exception
      */
     public static boolean install() throws JarInstallerException {
         return jarinstaller.impl.Utils.install(getJarPathAtBottomOfStack());
     }
     
     /**
-     * Returns true if a jar with the same name can be found in ~/.jars/jars
-     * 
-     * @return 
+     * Returns true if a jar with the same name and size can be found in ~/.jars/jars
+     *
+     * @return true if a jar with the same name exists
+     * @throws JarInstallerException if there is any exception
      */
     public static boolean isInstalled() throws JarInstallerException {
         return jarinstaller.impl.Utils.isInstalled(getJarPathAtBottomOfStack());
@@ -36,8 +37,9 @@ public class Api {
     
     /**
      * Returns true if it is run from a jar in ~/.jars/jars
-     * 
-     * @return 
+     *
+     * @return true if it is run from a jar in ~/.jars/jars
+     * @throws JarInstallerException if there is any exception
      */
     public static boolean isRunFromJarsDir() throws JarInstallerException {
         return jarinstaller.impl.Utils.isInJarsDirectory(getJarPathAtBottomOfStack());
